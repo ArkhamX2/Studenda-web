5cfe4c022a2375c647138b5a82d24dc21802e4e5
vadimyt <pvcs@yandex.ru>
2024-02-19 15:11:06 +0300

    autoComplete

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -11,21 +11,29 @@ import store from '../store'
 const LoginForm: FC = () => {
     const dispatch = useAppDispatch()
     const [loginInfo, setLoginInfo] = useState({ login: "", password: "" })
+    const [remember, setRemember] = useState(false);
     const LoginClick = async () => {
-        const url = "http://88.210.3.137/api/security/login"
-        const response = await axios({
-            method: "post",
-            url: url,
-            data: { email: loginInfo.login, password: loginInfo.password, rolename: "admin" }
-        })
-        dispatch(updateToken(response.data.Token))
+        if (loginInfo.login != "" && loginInfo.password != "")
+        {
+            const url = "http://88.210.3.137/api/security/login"
+            const response = await axios({
+                method: "post",
+                url: url,
+                data: { email: loginInfo.login, password: loginInfo.password, rolename: "admin" }
+            })
+            dispatch(updateToken(response.data.Token))
+        }
     }
     return (
         <main>
             <div style={{ alignSelf: 'center', display: 'flex', flexDirection: 'column', border: '2px solid lightgray', padding: '50px', color: '#371F76' }}>
                 <LoginLabel text='Введите ваш email'></LoginLabel>
-                <input placeholder='Логин' onChange={(e) => setLoginInfo({ ...loginInfo, login: e.target.value })}></input>
-                <input placeholder='Пароль' onChange={(e) => setLoginInfo({ ...loginInfo, password: e.target.value })}></input>
+                <input autoComplete='on' placeholder='Логин' onChange={(e) => setLoginInfo({ ...loginInfo, login: e.target.value })}></input>
+                <input autoComplete='on' type="password" placeholder='Пароль' onChange={(e) => setLoginInfo({ ...loginInfo, password: e.target.value })}></input>
+                <div>                
+                Запомнить меня:                
+                <input type="checkbox" onChange={()=>setRemember(!remember)} checked={remember} />
+                </div>
                 <LoginButton variant={ButtonVariant.primary} text='Войти' onClick={() => LoginClick()}></LoginButton>
             </div>
         </main>
