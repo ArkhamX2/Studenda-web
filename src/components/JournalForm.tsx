9aa3cb2ab37b2de4d6c363f4237ee8d9d6e6fea6
PolShestogo <99017989+PolShestogo@users.noreply.github.com>
2024-03-14 13:24:21 +0300

    prichesal_cod_2

--- src/components/JournalForm.tsx
+++ src/components/JournalForm.tsx
@@ -7,22 +7,17 @@ import AdminButton from './UI/button/AdminButton'
 const JournalForm: FC = () => {
 
     return (
-        <main style={{ display: 'flex', backgroundColor: 'white', maxHeight: '90svh', color: '#1B0E17', boxSizing: 'border-box' }}>
-            <div style={{ width: '200px', display: 'flex', flexDirection: 'column', border: '2px solid #490514', margin: '5px', padding: '10px', backgroundColor: '#F7F3F3', borderRadius: '5px' }}>
-                <div style={{ alignSelf: 'start', fontSize: '22px', fontWeight: '600', margin: '5px' }}>Журнал</div>
-                <div style={{ display: 'flex', flexDirection: 'column', margin: '10px 0px 5px 0px', borderLeft: '2px solid #8C2425', borderRadius: '5px', padding: '2px 5px', backgroundColor: '#F0EAE9', width: '100%' }}>
-                    <div style={{ width: '120px', alignSelf: 'start', fontSize: '20px', fontWeight: '600', margin: '5px' }}>Фильтр</div>
-                    <div><input type='checkbox'></input> Посещаемость </div>
-                    <div><input type='checkbox'></input> Оценки</div>
-
-
+        <main className={classes.Main}>
+            <div className={classes.NavigationBox}>
+                <div className={classes.Header}>Журнал</div>
+                <div className={classes.FilterBox}>
+                <div className={classes.FilterBoxLabel}>Фильтр:</div>
+                    <div><input type='checkbox' className={classes.CheckBox}></input> Посещаемость </div>
+                    <div><input type='checkbox' className={classes.CheckBox}></input> Оценки</div>
 
                 </div>
             </div>
-            <div style={{
-                width: '85%', border: '2px solid #490514', margin: '5px', overflowX: 'hidden', overflowY: 'auto', whiteSpace: 'nowrap',
-                backgroundColor: '#F7F3F3', borderRadius: '5px', scrollbarColor: "#490514", position: 'relative', padding: '5px'
-            }}>
+            <div className={classes.TableBox}>
                 <div style={{ marginLeft: '23.1em', overflowX: 'scroll', overflowY: 'visible', whiteSpace:'nowrap'}}>
                     <table className={classes.JournalTable}>
                         <tr className={classes.TableRow}>
@@ -156,10 +151,8 @@ const JournalForm: FC = () => {
                     </table>
                 </div>
 
-
             </div>
         </main>
-
     )
 }
 
